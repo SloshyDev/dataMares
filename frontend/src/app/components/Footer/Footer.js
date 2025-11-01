0e9a600bb6bc0bfb2de936bc6dda02135b59928c
SLOSHYMX <devlnicanorl@gmail.com>
2025-10-31 23:48:29 -0600

    fix: adjust logo sizes in footer for better responsiveness

--- frontend/src/app/components/Footer/Footer.js
+++ frontend/src/app/components/Footer/Footer.js
@@ -15,8 +15,8 @@ export default function Footer() {
                 <p className="text-sm mt-2 md:text-base">{t('copyright')}</p>
             </div>
             <Link href="/" className="block lg:order-2 my-2 lg:my-0">
-                <DmFullLogo className=" block lg:hidden h-8 w-auto mx-auto hover:scale-105 transition-transform duration-300" />
-                <DmMinLogo className="hidden lg:block 2xl:hidden h-8 w-auto mx-auto hover:scale-105 transition-transform duration-300" />
+                <DmFullLogo className="block lg:hidden 2xl:block h-8 lg:h-10 w-auto mx-auto hover:scale-105 transition-transform duration-300" />
+                <DmMinLogo className="hidden lg:block 2xl:hidden h-12 w-auto mx-auto hover:scale-105 transition-transform duration-300" />
             </Link>
             <Link href="/partners" className="block order-1 lg:order-3 my-2 lg:my-0">
                 <p className='bg-[#6a9a4a] dark:bg-[#265852] hover:dark:bg-[#1e7470] w-fit mx-auto hover:bg-[#1e7470] transition-colors duration-300 p-3 lg:p-4 text-lg lg:text-2xl text-white rounded-2xl'>{t('partners')}</p>

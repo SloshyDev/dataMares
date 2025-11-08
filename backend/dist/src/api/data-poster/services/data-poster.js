394d69210f6f93c9ce118de2f2586cb35156bca2
SLOSHYMX <devlnicanorl@gmail.com>
2025-11-08 13:30:19 -0600

    feat: add backend configuration files and data models for data-content and data-poster; implement controllers, services, and routes

---
+++ backend/dist/src/api/data-poster/services/data-poster.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * data-poster service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::data-poster.data-poster');
